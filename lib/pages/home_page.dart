02271bdaac0983202fb84c5eb399729a298ad82a
vipinsaini0 <vipin.ios14@gmail.com>
2021-05-17 13:00:13 +0530

    Day4 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,7 +8,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Demo"),
+        title: Text("Stone Buzz"),
       ),
       body: Center(
         child: Container(
